ba42ae8567d8ccdb9062b3161a56e3254c178209
Pedro Orgeira <poc.desarrollo@hotmail.com>
2017-05-11 19:45:32 +0200

    Fin

--- busqueda13/MainPage.xaml.cs
+++ busqueda13/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using Windows.Media.SpeechSynthesis;
+using Windows.UI.Xaml.Media.Imaging;
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -17,16 +21,54 @@ using Windows.UI.Xaml.Navigation;
 
 namespace busqueda13
 {
-    /// <summary>
-    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SpeechSynthesizer synthesizer;
+
         public MainPage()
         {
             this.InitializeComponent();
-
-
+            InicializaHabla();
         }
+
+        private async void BtnEmpieza_Click(object sender, RoutedEventArgs e)
+        {
+            await Dime("Hola, Cris; soy Alya. Me dijo Leidi Bag que estás buscando el código secreto que muestra el plano de Joc Moz. ¡Sé dónde lo guarda!: lo escondió encima del teclado de papá. Llévalo al ordenador de Joc Moz, y escanea el código para que te muestre el plano.");
+        }
+
+        private void InicializaHabla()
+        {
+            //lanza el habla
+            synthesizer = new SpeechSynthesizer();
+
+            VoiceInformation voiceInfo =
+         (
+           from voice in SpeechSynthesizer.AllVoices
+           where voice.Gender == VoiceGender.Female
+           select voice
+         ).FirstOrDefault() ?? SpeechSynthesizer.DefaultVoice;
+
+            synthesizer.Voice = voiceInfo;
+        }
+
+        private async Task Dime(String szTexto)
+        {
+            try
+            {
+                // crear el flujo desde el texto
+                SpeechSynthesisStream synthesisStream = await synthesizer.SynthesizeTextToStreamAsync(szTexto);
+
+                // ...y lo dice
+                media.AutoPlay = true;
+                media.SetSource(synthesisStream, synthesisStream.ContentType);
+                media.Play();
+            }
+            catch (Exception e)
+            {
+                var msg = new Windows.UI.Popups.MessageDialog(e.Message, "Error hablando:");
+                await msg.ShowAsync();
+            }
+        }
+
     }
 }
